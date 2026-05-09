5f3e9a3cbaed21d52081755a296d29558c4ef271
anand <anandmohan.amp@gmail.com>
2026-05-09 21:50:34 +0530

    chore: add Android project

---
+++ android/app/src/main/java/com/gcs/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.gcs
+
+import android.os.Bundle
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  override fun getMainComponentName(): String = "GCS"
+
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableImmersiveMode()
+  }
+
+  override fun onWindowFocusChanged(hasFocus: Boolean) {
+    super.onWindowFocusChanged(hasFocus)
+    if (hasFocus) {
+      enableImmersiveMode()
+    }
+  }
+
+  /**
+   * Hide both the status bar (clock/battery/signal) and the navigation bar
+   * for a true full-screen tactical canvas. We use the modern AndroidX
+   * `WindowInsetsControllerCompat` API, which is the API-30+ replacement
+   * for the deprecated `setSystemUiVisibility` flags. `BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE`
+   * means a user can still pull bars in temporarily with an edge swipe,
+   * but they auto-hide again — equivalent to "immersive sticky".
+   */
+  private fun enableImmersiveMode() {
+    WindowCompat.setDecorFitsSystemWindows(window, false)
+    WindowInsetsControllerCompat(window, window.decorView).apply {
+      hide(WindowInsetsCompat.Type.systemBars())
+      systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+    }
+  }
+}
